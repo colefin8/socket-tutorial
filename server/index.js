fe532a4458af3f5387a344a4b3f1a1106055e2c7
Cole Finlayson <cole.fin8@gmail.com>
2020-04-03 10:35:12 -0600

    first complete commit

--- server/index.js
+++ server/index.js
@@ -1,25 +1,30 @@
-require('dotenv').config();
-const express = require('express'),
-app = express(),
-socket = require('socket.io'),
-session = require('express-session');
-const {SERVER_PORT, SESSION_SECRET} = process.env;
+require("dotenv").config();
+const express = require("express"),
+  app = express(),
+  socket = require("socket.io"),
+  session = require("express-session");
+const { SERVER_PORT, SESSION_SECRET } = process.env;
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/../public`))
-app.use(session({
+app.use(express.static(`${__dirname}/../public`));
+app.use(
+  session({
     resave: false,
     saveUninitialized: true,
     secret: SESSION_SECRET,
     cookie: {
-        maxAge: 60 * 60 * 1000
+      maxAge: 60 * 60 * 1000
     }
-}))
-
-
+  })
+);
 
 const port = SERVER_PORT || 4040;
-const server = app.listen(port, ()=> {
-    console.log(`Server on ${port}`)
-})
+const server = app.listen(port, () => {
+  console.log(`Server on ${port}`);
+});
 
+const io = socket(server);
+
+io.on("connection", () => {
+  console.log("made socket connection");
+});
